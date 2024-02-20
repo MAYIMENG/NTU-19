5bb612443532bd22d0d10efaf1a917fba7543445
MAYIMENG <160451886+MAYIMENG@users.noreply.github.com>
2024-02-20 22:15:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
-def index():
-  return(render_template("index.html"))
+r = ""
+first_time = 1
 
-@app.route("/main",methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return (render_template("index.html"))
+
+
+@app.route("/main", methods=["GET", "POST"])
 def main():
-  r = request.form.get("r")
-  return(render_template("main.html",r=r))
+    global r, first_time
+    if first_time == 1:
+        r = request.form.get("r")
+        first_time = 0
+    return (render_template("main.html", r=r))
 
-@app.route("/",methods=["GET","POST"])
-def index():
-  return(render_template("image_gpt.html"))
 
+@app.route("/image_gpt", methods=["GET", "POST"])
+def image_gpt():
+    return (render_template("image_gpt.html"))
 
-if __name__=="__main__":
-  app.run()
+@app.route("/end", methods=["GET", "POST"])
+def end():
+    global first_time
+    first_time = 1
+    return (render_template("end.html"))
+
+if __name__ == "__main__":
+    app.run()
