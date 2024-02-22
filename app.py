07187711a4903f26452ea1e804dfefe707993dfb
MAYIMENG <160451886+MAYIMENG@users.noreply.github.com>
2024-02-22 10:23:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,74 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import replicate
 import os
 import time
 from openai import OpenAI
 
-openai_api_key=os.environ["OPENAI_API_TOKEN"]="sk-qx4zKKzHnYRc7k8LuXyKHXdQi0gf5BcisgXBVZcXc9t4K8E9"
-os.environ["REPLICATE_API_TOKEN"]="r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
 
-model = OpenAI(api_key="sk-qx4zKKzHnYRc7k8LuXyKHXdQi0gf5BcisgXBVZcXc9t4K8E9",base_url="https://api.chatanywhere.tech/v1")
 
+model = OpenAI(api_key="sk-qx4zKKzHnYRc7k8LuXyKHXdQi0gf5BcisgXBVZcXc9t4K8E9",
+               base_url="https://api.chatanywhere.tech/v1")
+os.environ["REPLICATE_API_TOKEN"] = "r8_GHyHGLZOntMYvtEgUZiE9vFdtMZrj7L2ZTtgM"
 
 app = Flask(__name__)
-
 
 r = ""
 first_time = 1
 
-@app.route("/",methods=["GET","POST"])
+# index
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return(render_template("index.html"))
+    return (render_template("index.html"))
 
-@app.route("/main",methods=["GET","POST"])
+#main
+@app.route("/main", methods=["GET", "POST"])
 def main():
-    global r,first_time
-    if first_time==1:
+    global r, first_time
+    if first_time == 1:
         r = request.form.get("r")
-        first_time=0
-    return(render_template("main.html",r=r))
+        first_time = 0
+    return (render_template("main.html", r=r))
 
-@app.route("/image_gpt",methods=["GET","POST"])
+#image
+@app.route("/image_gpt", methods=["GET", "POST"])
 def image_gpt():
-    return(render_template("image_gpt.html"))
+    return (render_template("image_gpt.html"))
 
-@app.route("/text_gpt",methods=["GET","POST"])
+@app.route("/image_result", methods=["GET", "POST"])
+def image_result():
+    q = request.form.get("q")
+    r = replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+        input={ 
+        "prompt": q,}
+)
+    time.sleep(5)
+    return (render_template("image_result.html",r=r[0]))
+
+#text
+@app.route("/text_gpt", methods=["GET", "POST"])
 def text_gpt():
-    return(render_template("text_gpt.html"))
+    return (render_template("text_gpt.html"))
 
-@app.route("/text_result",methods=["GET","POST"])
+@app.route("/text_result", methods=["GET", "POST"])
 def text_result():
     q = request.form.get("q")
-     r = model.chat.completions.create(
+    r = model.chat.completions.create(
     model="gpt-3.5-turbo", 
     messages=[
         {"role": "user",
          "content": q
         }
     ],
-    )
-    time.sleep(10)
-    return(render_template("text_result.html",r=r.choices[0].message.content))
+)
+    time.sleep(5)
+    return (render_template("text_result.html",r=r.choices[0].message.content))
 
-
-@app.route("/end",methods=["GET","POST"])
+# end
+@app.route("/end", methods=["GET", "POST"])
 def end():
     global first_time
     first_time = 1
-    return(render_template("end.html"))
+    return (render_template("end.html"))
 
 if __name__ == "__main__":
     app.run()
